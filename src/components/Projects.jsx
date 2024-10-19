c0c20745a14676e55f6585997cf1f9ed22c1cf8a
ale <alexlab84@hotmail.com>
2024-10-19 19:12:12 +0200

    Add changes in proyect component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,17 @@
+import meme from '../images/meme.png'
+import refran from '../images/refran.jpg'
+
 const projects = [
     {
-      title: 'Generador de memes',
-      description: 'Una aplicación para crear memes personalizados.',
-      image: 'url_del_meme_project_image',
-      githubLink: 'https://github.com/alexlab84/Generador-de-memes',
-      liveDemo: 'https://alexlab84.github.io/Generador-de-memes/',
+      title: 'Meme generator',
+      description: 'Aplicación para crear memes personalizados.',
+      image: meme,
+      liveDemo: 'https://alexlab84.github.io/Meme-Generator/',
     },
     {
       title: 'Generador de refranes',
       description: 'Genera refranes divertidos de forma aleatoria.',
-      image: 'url_del_refran_project_image',
+      image: refran,
       githubLink: 'https://github.com/alexlab84/Generador-de-refranes-divertidos',
       liveDemo: 'https://alexlab84.github.io/Generador-de-refranes-divertidos/',
     },
@@ -18,15 +20,16 @@ const projects = [
 
   function Projects() {
     return (
+     
       <section className="about">
         <h2 className="secondTitle">Proyectos</h2>
         <div className="projects-grid">
           {projects.map((project, index) => (
             <div key={index} className="project-card">
-              <img src={project.image} alt={project.title} />
               <h3>{project.title}</h3>
+              <img src={project.image} alt={project.title} className='imageProyect'/>
               <p className="paragraph">{project.description}</p>
-              <a href={project.githubLink} target="_blank" rel="noopener noreferrer">GitHub</a>
+              
               <a href={project.liveDemo} target="_blank" rel="noopener noreferrer">Demo en vivo</a>
             </div>
           ))}
