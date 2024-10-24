75d7cbb9eabe77e698e7f42b18c1c00d91e744b9
ale <alexlab84@hotmail.com>
2024-10-24 21:42:03 +0200

    Add changes in contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,8 @@
 function Contact() {
     return (
       <section className="contact">
-        <h2>Contacto</h2>
-        <p>¡Estoy abierta a nuevas oportunidades! No dudes en contactarme.</p>
+        <h2 className="secondTitle">Contacto</h2>
+        <p className="paragraph">¡Estoy abierta a nuevas oportunidades! No dudes en contactarme.</p>
         <ul>
           <li><a href="https://www.linkedin.com/in/alejandra-s%C3%A1nchez-3663bb300/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
           <li><a href="https://github.com/alexlab84" target="_blank" rel="noopener noreferrer">GitHub</a></li>
