84b7d7f2f7de0bd9a6dbb670bcafbe03bc2ba3f4
ale <alexlab84@hotmail.com>
2024-10-29 13:24:12 +0100

    Modifications to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
       <footer>
-        <p>&copy; 2024 Alejandra Sánchez. Todos los derechos reservados.</p>
+        <p>&copy; 2024 Alejandra Sánchez</p>
       </footer>
     );
   }
