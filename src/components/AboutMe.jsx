5e979c8cf2cc653170fa8932cfc4ec2b2c94e96e
ale <alexlab84@hotmail.com>
2024-11-08 22:43:59 +0100

    Add new project

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,7 +3,7 @@ function AboutMe() {
       <section className="about">
         <h2 className="secondTitle">Sobre mí</h2>
         <p className="paragraph">
-          ¡Hola! Soy Alejandra Sánchez, una desarrolladora Front-End Junior con pasión por el diseño y la programación. Tras haber completado un bootcamp de programación web en Adalab, decidí reinventar mi carrera para adentrarme en el mundo tech. Me encanta crear interfaces amigables y funcionales.
+          ¡Hola! Soy Alejandra Sánchez, una desarrolladora Fullstack Junior con pasión por el diseño y la programación. Tras haber completado un bootcamp de programación web en Adalab, decidí reinventar mi carrera para adentrarme en el mundo tech. Me encanta crear interfaces amigables y funcionales.
         </p>
       </section>
     );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,46 +1,60 @@
-import meme from '../images/meme.png'
+import meme from '../images/meme.jpg'
 import refran from '../images/refran.jpg'
 import cards from '../images/HarryPotter.png'
+import cholResults from '../images/cholResults.jpg'
 
 const projects = [
-    {
-      title: 'Meme generator',
-      description: 'Aplicación para crear memes personalizados.',
-      image: meme,
-      liveDemo: 'https://alexlab84.github.io/Meme-Generator/',
-    },
-    {
-      title: 'Proverb generator',
-      description: 'Genera refranes divertidos de forma aleatoria.',
-      image: refran,
-      githubLink: 'https://github.com/alexlab84/Generador-de-refranes-divertidos',
-      liveDemo: 'https://alexlab84.github.io/Generador-de-refranes-divertidos/',
-    },
-    {
-      title: 'Harry Potter Cards',
-      description: 'Aplicación que muestra personajes de Harry Potter y filtra',
-      image: cards,
-      liveDemo: 'https://alexlab84.github.io/HarryPotterCards/'
-    }
-    // Añadir más proyectos aquí
-  ];
-
-  function Projects() {
-    return (
-     
-      <section className="projects">
-        <h2 className="secondTitle">Proyectos</h2>
-        <div className="projectsFlex">
-          {projects.map((project, index) => (
-            <div key={index} className="project-card">
-              <h3 className='projectsTitle'>{project.title}</h3>
-              <img src={project.image} alt={project.title} className='imageProyect'/>
-              <a className="linkProjects" href={project.liveDemo} target="_blank" rel="noopener noreferrer">Demo en vivo</a>
-            </div>
-          ))}
-        </div>
-      </section>
-    );
+  {
+    title: 'Meme generator',
+    description: 'Aplicación para crear memes personalizados.',
+    image: meme,
+    liveDemo: 'https://alexlab84.github.io/Meme-Generator/',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'React'], // Tecnologías utilizadas
+  },
+  {
+    title: 'Generador de refranes',
+    description: 'Genera refranes divertidos de forma aleatoria.',
+    image: refran,
+    githubLink: 'https://github.com/alexlab84/Generador-de-refranes-divertidos',
+    liveDemo: 'https://alexlab84.github.io/Generador-de-refranes-divertidos/',
+    technologies: ['HTML', 'CSS', 'JavaScript'], // Tecnologías utilizadas
+  },
+  {
+    title: 'Harry Potter Cards',
+    description: 'Aplicación que muestra personajes de Harry Potter y filtra.',
+    image: cards,
+    liveDemo: 'https://alexlab84.github.io/HarryPotterCards/',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'React'], // Tecnologías utilizadas
+  },
+  {
+    title: 'Cholesterol results',
+    description: 'Interfaz gráfica que muestra resultados de colesterol.',
+    image: cholResults,
+    liveDemo: 'https://alexlab84.github.io/Visualization-of-cholesterol-test-results/',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'React'], 
   }
+];
+
+function Projects() {
+  return (
+    <section className="projects">
+      <h2 className="secondTitle">Proyectos</h2>
+      <div className="projectsFlex">
+        {projects.map((project, index) => (
+          <div key={index} className="project-card">
+            <h3 className='projectsTitle'>{project.title}</h3>
+            <img src={project.image} alt={project.title} className='imageProyect'/>
+            <p className="project-description">{project.description}</p>
+            <p className="project-technologies">
+              <strong>Tecnologías utilizadas:</strong> {project.technologies.join(', ')}
+            </p>
+            <a className="linkProjects" href={project.liveDemo} target="_blank" rel="noopener noreferrer">Demo</a>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
   
   export default Projects;
